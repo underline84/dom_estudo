64dd63d4557006110bb3eb43e0fe9455b80e1764
underline84 <evanfatu@hotmail.com>
2021-10-06 21:24:22 -0300

    first commit

---
+++ listaDeTarefas.js
@@ -0,0 +1,21 @@
+const novaTarefa = document.querySelector('[data-form-button]');
+
+const criarTarefa = (evento)=>{
+    evento.preventDefault();
+    const lista = document.querySelector('[data-list]');
+    const input = document.querySelector('[data-form-input]');
+    const valor = input.value;
+    
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+
+    const conteudo = `<p class="content">${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+    
+    lista.appendChild(tarefa);
+  
+    input.value = "";
+}
+
+novaTarefa.addEventListener('click', criarTarefa);
